519fb3f7ebec6fd6ddf3af4a2379e09e159591b1
Jason Crawford <jason@jasoncrawford.org>
2014-10-17 23:12:40 -0700

    Refactor a bit

--- angellist.js
+++ angellist.js
@@ -6,16 +6,17 @@ var picSelector = '.pic img';
 var anonymousName = 'A Candidate';
 var anonymousImgSrc = 'https://angel.co/images/shared/nopic.png';
 
-var candidatesContent = document.getElementById(candidatesContentId);
-
-if (candidatesContent) {
-  candidatesContent.addEventListener('DOMNodeInserted', function (event) {
-    var candidateRows = document.querySelectorAll(candidateRowSelector);
-    Array.prototype.forEach.call(candidateRows, function (row) {
-      var name = row.querySelector(nameSelector);
-      var pic = row.querySelector(picSelector);
-      name.textContent = anonymousName;
-      pic.setAttribute('src', anonymousImgSrc);
-    })
+function anonymize() {
+  var candidateRows = document.querySelectorAll(candidateRowSelector);
+  Array.prototype.forEach.call(candidateRows, function (row) {
+    var name = row.querySelector(nameSelector);
+    var pic = row.querySelector(picSelector);
+    name.textContent = anonymousName;
+    pic.setAttribute('src', anonymousImgSrc);
   })
+}
+
+var candidatesContent = document.getElementById(candidatesContentId);
+if (candidatesContent) {
+  candidatesContent.addEventListener('DOMNodeInserted', anonymize);
 }
