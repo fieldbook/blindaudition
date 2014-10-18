4f44dada947324e87a595fc7b63191d406dab6d4
Jason Crawford <jason@jasoncrawford.org>
2014-10-17 22:04:35 -0700

    Basic working code for AngelList
    Replaces all names with “A Candidate” and all pics with the nopic icon

---
+++ angellist.js
@@ -0,0 +1,19 @@
+var candidatesContentId = 'candidates_content';
+var candidateRowSelector = '.candidate-row';
+var nameSelector = '.candidate-name a';
+var picSelector = '.pic img';
+
+var anonymousName = 'A Candidate';
+var anonymousImgSrc = 'https://angel.co/images/shared/nopic.png';
+
+var candidatesContent = document.getElementById(candidatesContentId);
+
+candidatesContent.addEventListener('DOMNodeInserted', function (event) {
+  var candidateRows = document.querySelectorAll(candidateRowSelector);
+  Array.prototype.forEach.call(candidateRows, function (row) {
+    var name = row.querySelector(nameSelector);
+    var pic = row.querySelector(picSelector);
+    name.textContent = anonymousName;
+    pic.setAttribute('src', anonymousImgSrc);
+  })
+})
