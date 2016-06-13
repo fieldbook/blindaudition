f78893c81be7ff9f0dc4efc8c5b5ea971ab5aae3
Jason Crawford <jason@jasoncrawford.org>
2016-06-12 17:58:22 -0700

    Add a blinder for LinkedIn profile pages

---
+++ linkedin.js
@@ -0,0 +1,11 @@
+blind({
+  container: '#body',
+  displayTypes: [
+    {
+      element: '.profile-card',
+      name: '.full-name',
+      pic: '.profile-picture img',
+    },
+  ],
+  anonymousImgSrc: 'https://static.licdn.com/scds/common/u/img/icon/icon_no_photo_no_border_30x30_v2.png',
+});
