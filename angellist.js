6c5e0b9a647e844cbf26f343155e33e85d0954f0
Jason Crawford <jason@jasoncrawford.org>
2016-06-12 21:43:37 -0700

    Add comments to AngelList and LinkedIn blinders

--- angellist.js
+++ angellist.js
@@ -1,8 +1,11 @@
 var blinder = new Blinder({anonymousImgSrc: 'https://angel.co/images/shared/nopic.png'});
 
+// Blinds the search results on, e.g., https://angel.co/candidates
 blinder.blind('#candidates_content', function () {
   this.blindElements('.card-content-container', function () {
     this.blindName('.js-browse-table-row-name a');
     this.blindPic('.candidate-picture img');
   })
 })
+
+// TODO: Profile pages (e.g., https://angel.co/jasoncrawford)
--- linkedin.js
+++ linkedin.js
@@ -2,9 +2,16 @@ var blinder = new Blinder({
   anonymousImgSrc: 'https://static.licdn.com/scds/common/u/img/icon/icon_no_photo_no_border_30x30_v2.png',
 })
 
+// Blinds a profile page (e.g., https://www.linkedin.com/in/jasonc)
 blinder.blind('#body', function () {
   this.blindElements('.profile-card', function () {
     this.blindName('.full-name');
     this.blindPic('.profile-picture img');
   })
+
+  // TODO:
+  // Replace document.title
+  // Replace candidate name and image in various other places throughout the page
 })
+
+// TODO: Other page types, such as search results
