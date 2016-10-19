2046102917450f3755463d23927e8e345c611ab8
Jason Crawford <jason@jasoncrawford.org>
2016-10-18 19:41:32 -0700

    Blind A-List body instead of .js-candidates
    The .js-candidates element was getting destroyed on navigation, so blinding
    stopped when you navigate. Need to attach to a persistent element, body will
    do

--- sites/alist.js
+++ sites/alist.js
@@ -1,7 +1,7 @@
 var blinder = new Blinder({anonymousImgSrc: 'https://angel.co/images/shared/nopic.png'});
 
 // Blinds the search results on alist.co
-blinder.blind('.js-candidates', function () {
+blinder.blind('body', function () {
   this.blindElements('.candidateProfileRow', function () {
     this.blindName('.candidateProfileRow-name');
     this.blindPic('img.candidateProfileRow-profileImage');
